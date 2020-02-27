749222b2a9a5c4ca0fcfa73953e157d8d45c6fb5
iomesome <iomesome@gmail.com>
2020-02-27 08:56:05 +0800

    highlight work

--- static/js/main.js
+++ static/js/main.js
@@ -4,6 +4,93 @@ $(document).ready(function() {
     // var reg_text = $('#reg_text');
     // var reg_expression = $('#reg_expression');
 
+    //http://jsfiddle.net/8mdX4/1211/
+    function getTextNodesIn(node) {
+        var textNodes = [];
+        if (node.nodeType == 3) {
+            textNodes.push(node);
+        } else {
+            var children = node.childNodes;
+            for (var i = 0, len = children.length; i < len; ++i) {
+                textNodes.push.apply(textNodes, getTextNodesIn(children[i]));
+            }
+        }
+        return textNodes;
+    }
+    
+    function setSelectionRange(el, start, end) {
+        if (document.createRange && window.getSelection) {
+            var range = document.createRange();
+            range.selectNodeContents(el);
+            var textNodes = getTextNodesIn(el);
+            var foundStart = false;
+            var charCount = 0, endCharCount;
+    
+            for (var i = 0, textNode; textNode = textNodes[i++]; ) {
+                endCharCount = charCount + textNode.length;
+                if (!foundStart && start >= charCount && (start < endCharCount || (start == endCharCount && i <= textNodes.length))) {
+                    range.setStart(textNode, start - charCount);
+                    foundStart = true;
+                }
+                if (foundStart && end <= endCharCount) {
+                    range.setEnd(textNode, end - charCount);
+                    break;
+                }
+                charCount = endCharCount;
+            }
+    
+            var sel = window.getSelection();
+            sel.removeAllRanges();
+            sel.addRange(range);
+        } else if (document.selection && document.body.createTextRange) {
+            var textRange = document.body.createTextRange();
+            textRange.moveToElementText(el);
+            textRange.collapse(true);
+            textRange.moveEnd("character", end);
+            textRange.moveStart("character", start);
+            textRange.select();
+        }
+    }
+    
+    function makeEditableAndHighlight(colour) {
+        sel = window.getSelection();
+        if (sel.rangeCount && sel.getRangeAt) {
+            range = sel.getRangeAt(0);
+        }
+        document.designMode = "on";
+        if (range) {
+            sel.removeAllRanges();
+            sel.addRange(range);
+        }
+        // Use HiliteColor since some browsers apply BackColor to the whole block
+        if (!document.execCommand("HiliteColor", false, colour)) {
+            document.execCommand("BackColor", false, colour);
+        }
+        document.designMode = "off";
+    }
+    
+    function highlight(colour) {
+        var range, sel;
+        if (window.getSelection) {
+            // IE9 and non-IE
+            try {
+                if (!document.execCommand("BackColor", false, colour)) {
+                    makeEditableAndHighlight(colour);
+                }
+            } catch (ex) {
+                makeEditableAndHighlight(colour)
+            }
+        } else if (document.selection && document.selection.createRange) {
+            // IE <= 8 case
+            range = document.selection.createRange();
+            range.execCommand("BackColor", false, colour);
+        }
+    }
+    
+    function selectAndHighlightRange(id, start, end) {
+        setSelectionRange(document.getElementById(id), start+1, end+1);
+        highlight("yellow");
+    }
     
 
 
@@ -12,7 +99,7 @@ $(document).ready(function() {
 
     $('#recognize').click(function() {
         document.getElementById("info").innerHTML = "Begin to process!"
-        reg_expression = document.getElementById("reg_expression").value
+        reg_expression = document.getElementById("reg_expression1").value
         reg_text = document.getElementById("reg_text").value;
         
 
@@ -29,8 +116,34 @@ $(document).ready(function() {
             return ;
         }
  
+         
+        $.getJSON($SCRIPT_ROOT + '_recognize', {
+            reg_expression  :   reg_expression,
+            reg_text        :   reg_text
+        }, function(data){
 
-        document.getElementById("info").innerHTML = "Finish to process!"
+            var r = data['result']['result'].toString() ;
+            while(r.search('\n') != -1 )
+            {
+                r = r.replace('\n','<br/>');
+            }
+            document.getElementById("reg_result").innerHTML = reg_text + '<br/>' + r;
+
+           for (var h in data['result']['highlight'])
+           {
+            // alert(data['result']['highlight'][h].start.toString() )
+            selectAndHighlightRange('reg_result',
+            parseInt(data['result']['highlight'][h].start), 
+            parseInt(data['result']['highlight'][h].end)
+            )
+           }
+
+
+           document.getElementById("info").innerHTML = "Finish to process!"
+            // document.getElementById("reg_result").innerHTML = data['result']['result'].toString() 
+        });
+
+
         // num2.val(mainOutput.html());
         // $.getJSON($SCRIPT_ROOT + '/_calculate', {
         //     number1: num1.val(),
--- webRecognize.py
+++ webRecognize.py
@@ -6,9 +6,108 @@ from datetime import timedelta
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = timedelta(seconds=1)
 
+
+
+
+
+
+dirDict = {'North': 'N', 'South': 'S', 'West': 'W', 'East': 'E'}
+
+def search(regex, text):
+    result = ""
+    errorMessage = ""
+
+    # regex= r"\W+(?P<dir1>North|South|West|East)\W+(?P<degrees>\d+)\W+degrees\W+(?P<minutes>\d+)\W+minutes\W+(?P<dir2>North|South|West|East)(?:.|\n)*?distance of\W+(?P<feet>\d+.?\d+)"
+
+    m = re.findall(regex, text, re.IGNORECASE)
+    strm = map(str, m)
+    # messagebox.showinfo(title = "The count of the result is " + str(len(m)), message = " The content of the result is: \n" + '\n'.join(strm) )
+    
+    resultList = []
+    i = 0
+    for matches in re.finditer(regex, text, re.IGNORECASE):
+        i = i + 1
+        # messagebox.showinfo(message = str(matches.groupdict()))
+        gd = matches.groupdict()
+        resultList.append({'start': matches.start(), 'end': matches.end(), 'index' : i})
+        # messagebox.showinfo(message = "The begin and end pos of the mathch is : " + str(matches.start())+ "-" + str(matches.end()))
+        
+
+        # if "dir1" in gd:
+        #     messagebox.showinfo(message = "has dir1")
+        # else :
+        #     messagebox.showinfo(message = "doesn't have dir1")
+
+        # matches.groupdict
+        dir1 = ''
+        if matches.group('dir1') is None :
+            # messagebox.showinfo(message='dir1 is none')
+            errorMessage += "dir1 is none"
+        else:
+            dir1 = matches.group('dir1')
+            # resultList.append({'start': matches.start(), 'end': matches.end(), 'index' : i})
+       
+ 
+            
+
+        degrees = ''
+        if matches.group('degrees') is None :
+            # messagebox.showinfo(message='degrees is none')
+            errorMessage += "degrees is none"
+        else:
+            degrees = matches.group('degrees')
+            # g = degrees
+            # resultList.append({'start': g.start(), 'end': g.end(), 'index' : i})
+  
+        minutes = ''
+        if matches.group('minutes') is None :
+            errorMessage += "minutes is none"
+        else:
+            minutes = matches.group('minutes')
+            # g = minutes
+            # resultList.append({'start': g.start(), 'end': g.end(), 'index' : i})
+
+        seconds = "00"
+
+
+        dir2 = ''
+        if matches.group('dir2') is None :
+            errorMessage += "dir2 is none"
+        else:
+            dir2 = dirDict[matches.group('dir2')]
+            # g = dir2
+            # resultList.append({'start': g.start(), 'end': g.end(), 'index' : i})
+
+        feet = ''
+        if matches.group('feet') is None :
+           errorMessage += "feet is none"
+        else:
+            feet = matches.group('feet')
+            # g = feet
+            # resultList.append({'start': g.start(), 'end': g.end(), 'index' : i})
+
+
+        result = result + "DD " + dir1 + " " + degrees + "-" + minutes + "-" + seconds + " " + dir2 + " " + feet + '\n'
+    return {'result' : result, 'highlight' : resultList}
+
+
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/_recognize')
+def recognize():
+    x = '\W+(?P<dir1>South|North|West|East)\W+(?P<degrees>\d+)\W+degrees?\W+(?P<minutes>\d+)\W+minutes?\W+[(?P<seconds>\d+)\W+seconds?\W+]?(?P<dir2>South|North|West|East)(.|\n)*?(?P<distance>\d+.\d+)\W+feet'
+    reg_expression = request.args.get('reg_expression', '')
+    reg_text = request.args.get('reg_text', '')
+    
+    result = search(reg_expression, reg_text)
+
+    return jsonify(result = result)
+
 
 
 if __name__ == '__main__':
